2ea9bb20052c86d2d0515d4cf48c5ca8d4d56579
nitish6174 <nitish.garg.6174@gmail.com>
2016-07-08 17:59:25 +0530

    Corrected alignment settings, added options and sections

--- js/editor.js
+++ js/editor.js
@@ -1,202 +1,258 @@
-spaceList = document.querySelectorAll('.space');
-for(var i=0;i<spaceList.length;i++)
-{
-	var count = spaceList[i].dataset.spaces;
-	for(var j=0;j<count;j++)
-		spaceList[i].innerHTML += '&nbsp';
-}
+document.querySelector('#page').contentEditable = true;
+
+
+
+$('#page').addClass('droid');
+$('.section-title').addClass('ruled');
+$('.section-title').addClass('rule-above');
+
+$('#minorHide').addClass('selected');
+$('#contact4').addClass('selected');
+$('#margin4').addClass('selected');
+
+defaultTemplateVars = [ "fontDroid" , "caseNormal" , "titleRuled" , "ruleAbove" , "imageShow" , "rollShow" , "course1" , "tableShow" , "edyearFirst" , "experience1" , "projects1" ];
+for(i=0;i<defaultTemplateVars.length;i++)
+	$('#'+defaultTemplateVars[i]).addClass('selected');
+
 
 $('.toggle-option').click(function(){
-	if(!$(this).hasClass('selected'))
+	toggleType = $(this).attr('data-toggle');
+	toggleValue = $(this).attr('id');
+	if(!$(this).hasClass('multi-select'))
 	{
-		toggleType = $(this).attr('data-toggle');
-		toggleValue = $(this).attr('id');
-		switch(toggleType)
+		if(!$(this).hasClass('selected'))
 		{
-			case 'minor':
-				if(toggleValue=='minorShow')
-				{
-					$('#contentMinor').show();
-					$('#image_box').css('margin-top','35px');
-				}
-				else
-				{
-					$('#contentMinor').hide();
-					$('#image_box').css('margin-top','30px');
-				}
-				break;
-			case 'contact':
-				if(toggleValue=='contact3')
-				{
-					$('#contactLink1').hide();
-					$('#contactLink2').hide();
-					$('#contact').css('margin-top','45px');
-				}
-				else if(toggleValue=='contact4')
-				{
-					$('#contactLink1').show();
-					$('#contactLink2').hide();
-					$('#contact').css('margin-top','35px');
-				}
-				else
-				{
-					$('#contactLink1').show();
-					$('#contactLink2').show();
-					$('#contact').css('margin-top','25px');
-				}
-				break;
-
-			case 'font':
-				if(toggleValue=='fontSans')
-				{
-					$('#page').removeClass('serif');
-					$('#page').addClass('sans');
-				}
-				else
-				{
-					$('#page').removeClass('sans');
-					$('#page').addClass('serif');
-				}
-				break;
-			case 'case':
-				if(toggleValue=='caseNormal')
-					$('.section-title').removeClass('uppercase');
-				else
-					$('.section-title').addClass('uppercase');
-				break;
-			case 'title':
-				if(toggleValue=='titleRuled')
-				{
-					$('.section-title').removeClass('shaded');
-					$('.section-title').addClass('ruled');
-				}
-				else
-				{
-					$('.section-title').removeClass('ruled');
-					$('.section-title').addClass('shaded');
-				}
-				break;
-			case 'rule':
-				if(toggleValue=='ruleAbove')
-				{
-					$('.section-title').removeClass('rule-below');
-					$('.section-title').addClass('rule-above');
-				}
-				else
-				{
-					$('.section-title').removeClass('rule-above');
-					$('.section-title').addClass('rule-below');
-				}
-				break;
-
-			case 'image':
-				if(toggleValue=='imageShow')
-				{
-					$('#image_box').show();
-					$('#info').css('margin-left','0px');
-				}
-				else
-				{
-					$('#image_box').hide();
-					$('#info').css('margin-left','20px');
-				}
-				break;
-			case 'roll':
-				if(toggleValue=='rollShow')
-				{
-					$('#contentRoll').show();
-					$('#info').css('margin-top','0px');
-				}
-				else
-				{
-					$('#contentRoll').hide();
-					$('#info').css('margin-top','10px');
-				}
-				break;
-			case 'table':
-				if(toggleValue=='tableShow')
-				{
-					$('#educationTable').removeClass('borderless');
-					$('#educationTable').addClass('customBordered');
-				}
-				else
-				{
-					$('#educationTable').removeClass('customBordered');
-					$('#educationTable').addClass('borderless');
-				}
-				break;
-			case 'edyear':
-				if(toggleValue=='edyearFirst')
-				{
-					$("#educationTable tr").each(function () {
-						$(this).find("td").eq(0).before($(this).find("td").eq(3));
-					});
-				}
-				else
-				{
-					$("#educationTable tr").each(function () {
-						$(this).find("td").eq(3).after($(this).find("td").eq(0));
-					});
-				}
-				break;
-			case 'experience':
-				if(toggleValue=='experience1')
-				{
-					$("#sectionExperience .title , #sectionExperience .time").css('display','block');
-					$("#sectionExperience .time").removeClass('right').addClass('tab');
-					$("#sectionExperience .link").hide();
-				}
-				else
-				{
-					$("#sectionExperience .title , #sectionExperience .time").css('display','inline-block');
-					$("#sectionExperience .time").addClass('right').removeClass('tab');
-					$("#sectionExperience .link").show();
-				}
-				break;
-			case 'projects':
-				if(toggleValue=='projects1')
-				{
-					$("#sectionProjects .title , #sectionProjects .time").css('display','block');
-					$("#sectionProjects .time").removeClass('right');
-					$("#sectionProjects .mentor , #sectionProjects .link").hide();
-				}
-				else
-				{
-					$("#sectionProjects .title , #sectionProjects .time").css('display','inline-block');
-					$("#sectionProjects .time").addClass('right');
-					$("#sectionProjects .mentor , #sectionProjects .link").show();
-				}
-				break;
+			$('.toggle-option',$(this).parent()).removeClass('selected');
+			$(this).addClass('selected');
+			changeTemplate(toggleType,toggleValue);
 		}
-		$('.toggle-option[data-toggle="'+toggleType+'"]').removeClass('selected');
-		$(this).addClass('selected');
+	}
+	else
+	{
+		if($(this).hasClass('selected'))
+			$(this).removeClass('selected');
+		else
+			$(this).addClass('selected');
+		changeTemplate(toggleType,toggleValue);
 	}
 });
 
 
-$('#page').addClass('serif');
-$('.section-title').addClass('ruled');
-$('.section-title').addClass('rule-above');
-$("#sectionExperience .link").hide();
-$("#sectionExperience .time , #sectionProjects .time").addClass('tab');
-$("#sectionProjects .mentor , #sectionProjects .link").hide();
 
+function template(value)
+{
+	if(value=='default')
+	{
+		$('#defaultTemplateBtn').removeClass('btn-default').addClass('btn-danger');
+		$('#customTemplateBtn').removeClass('btn-danger').addClass('btn-default');
+		$('#customTemplateOptions').hide();
+		for(i=0;i<defaultTemplateVars.length;i++)
+			$('#'+defaultTemplateVars[i]).click();
+	}
+	else
+	{
+		$('#customTemplateBtn').removeClass('btn-default').addClass('btn-danger');
+		$('#defaultTemplateBtn').removeClass('btn-danger').addClass('btn-default');
+		$('#customTemplateOptions').show();
+	}
+}
 
-$('#minorShow').addClass('selected');
-$('#contact5').addClass('selected');
+function changeTemplate(toggleType,toggleValue)
+{
+	switch(toggleType)
+	{
+		case 'minor':
+			if(toggleValue=='minorShow')
+			{
+				$('#contentMinor').show();
+				$('#image_box').css('margin-top','35px');
+			}
+			else
+			{
+				$('#contentMinor').hide();
+				$('#image_box').css('margin-top','25px');
+			}
+			break;
+		case 'contact':
+			if(toggleValue=='contact3')
+			{
+				$('#contactLink1').hide();
+				$('#contactLink2').hide();
+			}
+			else if(toggleValue=='contact4')
+			{
+				$('#contactLink1').show();
+				$('#contactLink2').hide();
+			}
+			else
+			{
+				$('#contactLink1').show();
+				$('#contactLink2').show();
+			}
+			break;
+		case 'margin':
+			if(toggleValue=='margin1')
+				$('#page').css('padding','0.2cm 1cm 1cm 1cm');
+			else if(toggleValue=='margin2')
+				$('#page').css('padding','0.2cm 1.1cm 1cm 1.1cm');
+			else if(toggleValue=='margin3')
+				$('#page').css('padding','0.2cm 1.2cm 1cm 1.2cm');
+			else if(toggleValue=='margin4')
+				$('#page').css('padding','0.2cm 1.3cm 1cm 1.3cm');
+			else if(toggleValue=='margin5')
+				$('#page').css('padding','0.2cm 1.4cm 1cm 1.4cm');
+			else if(toggleValue=='margin6')
+				$('#page').css('padding','0.2cm 1.5cm 1cm 1.5cm');
+			break;
+		case 'column':
+			if(toggleValue=='column1')
+				$('.table tbody tr td:nth-child(1)').toggleClass('text-center');
+			else if(toggleValue=='column2')
+				$('.table tbody tr td:nth-child(2)').toggleClass('text-center');
+			else if(toggleValue=='column3')
+				$('.table tbody tr td:nth-child(3)').toggleClass('text-center');
+			else if(toggleValue=='column4')
+				$('.table tbody tr td:nth-child(4)').toggleClass('text-center');
+			break;
 
-$('#fontSerif').addClass('selected');
-$('#caseNormal').addClass('selected');
-$('#titleRuled').addClass('selected');
-$('#ruleAbove').addClass('selected');
+		case 'font':
+			if(toggleValue=='fontDroid')
+				$('#page').removeClass('verdana').removeClass('roboto').addClass('droid');
+			else if(toggleValue=='fontVerdana')
+				$('#page').removeClass('droid').removeClass('roboto').addClass('verdana');
+			else if(toggleValue=='fontRoboto')
+				$('#page').removeClass('verdana').removeClass('droid').addClass('roboto');
+			break;
+		case 'case':
+			if(toggleValue=='caseNormal')
+				$('.section-title').removeClass('uppercase');
+			else
+				$('.section-title').addClass('uppercase');
+			break;
+		case 'title':
+			if(toggleValue=='titleRuled')
+			{
+				$('.section-title').removeClass('shaded');
+				$('.section-title').addClass('ruled');
+			}
+			else
+			{
+				$('.section-title').removeClass('ruled');
+				$('.section-title').addClass('shaded');
+			}
+			break;
+		case 'rule':
+			if(toggleValue=='ruleAbove')
+			{
+				$('.section-title').removeClass('rule-below');
+				$('.section-title').addClass('rule-above');
+			}
+			else
+			{
+				$('.section-title').removeClass('rule-above');
+				$('.section-title').addClass('rule-below');
+			}
+			break;
 
-$('#imageShow').addClass('selected');
-$('#rollShow').addClass('selected');
-$('#tableShow').addClass('selected');
-$('#edyearFirst').addClass('selected');
-$('#experience1').addClass('selected');
-$('#projects1').addClass('selected');
-
-
-
-document.querySelector('#page').contentEditable = true;
+		case 'image':
+			if(toggleValue=='imageShow')
+			{
+				$('#image_box').show();
+				$('#info').css('margin-left','0px');
+			}
+			else
+			{
+				$('#image_box').hide();
+				$('#info').css('margin-left','20px');
+			}
+			break;
+		case 'roll':
+			if(toggleValue=='rollShow')
+			{
+				$('#contentRoll').show();
+				$('#info').css('margin-top','0px');
+			}
+			else
+			{
+				$('#contentRoll').hide();
+				$('#info').css('margin-top','10px');
+			}
+			break;
+		case 'course':
+			if(toggleValue=='course1')
+			{
+				$('#contentBranch').hide();
+				$('#contentCourse').text('Branch - XXX Engineering');
+			}
+			else
+			{
+				$('#contentBranch').show();
+				$('#contentCourse').text('Branch undergraduate');
+			}
+			break;
+		case 'table':
+			if(toggleValue=='tableShow')
+			{
+				$('#educationTable').removeClass('borderless');
+				$('#educationTable').addClass('customBordered');
+			}
+			else
+			{
+				$('#educationTable').removeClass('customBordered');
+				$('#educationTable').addClass('borderless');
+			}
+			break;
+		case 'edyear':
+			if(toggleValue=='edyearFirst')
+			{
+				$("#educationTable tr").each(function () {
+					$(this).find("td").eq(0).before($(this).find("td").eq(3));
+				});
+				var temp = document.getElementById('column4').className;
+				document.getElementById('column4').className = document.getElementById('column3').className;
+				document.getElementById('column3').className = document.getElementById('column2').className;
+				document.getElementById('column2').className = document.getElementById('column1').className;
+				document.getElementById('column1').className = temp;
+			}
+			else
+			{
+				$("#educationTable tr").each(function () {
+					$(this).find("td").eq(3).after($(this).find("td").eq(0));
+				});
+				var temp = document.getElementById('column1').className;
+				document.getElementById('column1').className = document.getElementById('column2').className;
+				document.getElementById('column2').className = document.getElementById('column3').className;
+				document.getElementById('column3').className = document.getElementById('column4').className;
+				document.getElementById('column4').className = temp;
+			}
+			break;
+		case 'experience':
+			if(toggleValue=='experience1')
+			{
+				$("#sectionExperience .title , #sectionExperience .time").css('display','inline-block');
+				$("#sectionExperience .time").addClass('right').removeClass('tab');
+				$("#sectionExperience .link").show();
+			}
+			else
+			{
+				$("#sectionExperience .title , #sectionExperience .time").css('display','block');
+				$("#sectionExperience .time").removeClass('right').addClass('tab');
+				$("#sectionExperience .link").hide();
+			}
+			break;
+		case 'projects':
+			if(toggleValue=='projects1')
+			{
+				$("#sectionProjects .title , #sectionProjects .time").css('display','inline-block');
+				$("#sectionProjects .time").addClass('right').removeClass('tab');
+				$("#sectionProjects .mentor , #sectionProjects .link").show();
+			}
+			else
+			{
+				$("#sectionProjects .title , #sectionProjects .time").css('display','block');
+				$("#sectionProjects .time").removeClass('right').addClass('tab');
+				$("#sectionProjects .mentor , #sectionProjects .link").hide();
+			}
+			break;
+	}
+}
