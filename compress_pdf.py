05ec5240c46992202d7f326d0e015a16034534b4
nitish6174 <nitish.garg.6174@gmail.com>
2016-07-27 14:00:47 +0530

    Modified compression script and added instructions

--- compress_pdf.py
+++ compress_pdf.py
@@ -1,24 +1,16 @@
 import os
 import sys
 
-# dir_path = "/absolute/path/to/directory/"
-dir_path = "/mnt/B4A49A87A49A4BAC/Downloads/"
+dir_path = "/absolute/path/to/directory/"
 # Make sure dir_path is ending in /
 num_of_pages = 2
-# out_file = dir_path+"14010XXXX_CV.pdf"
-out_file = dir_path+"140101045_CV.pdf"
+out_file = dir_path+"14010XXXX_CV.pdf"
 
-
-IS_PY3 = sys.version_info[0] == 3
-if IS_PY3:
-	_range = range
-else:
-	_range = xrange
 
 cmd = "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="+out_file
 # cmd = "gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile="+out_file
-for i in _range(num_of_pages):
-	filename = dir_path+str(i+1)+".pdf"
+for i in range(1,num_of_pages+1):
+	filename = dir_path+str(i)+".pdf"
 	cmd = cmd+" "+filename
 
 os.system(cmd)
